dea7763f650810fabe670429bbb49d37c3d035fc
Gerome Heath <gerome247@gmail.com>
2021-08-18 13:50:00 +0100

    fixed eslint hook checking

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Pet from './Pet';
+import SearchParams from './SearchParams';
 import { render } from 'react-dom';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
